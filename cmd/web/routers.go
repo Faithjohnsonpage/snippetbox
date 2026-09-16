9484511b16b4e9cf400b46f17124a9574f950c79
Faith Johnson <ermac_4life@yahoo.com>
2026-09-16 12:33:59 +0100

    Composable middleware chains

--- cmd/web/routers.go
+++ cmd/web/routers.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 // The routes() method returns a servemux containing our application routes initially.
 // Update the signature for the routes() method so that it returns a
@@ -27,5 +31,11 @@ func (app *application) routes() http.Handler {
 	//return app.logRequest(commonHeaders(mux))
 
 	// Wrap the existing chain with the recoverPanic middleware.
-	return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+	//return app.recoverPanic(app.logRequest(commonHeaders(mux)))
+
+	// Create a middleware chain containing our 'standard' middleware
+	// which will be used for every request our application receives.
+	standard := alice.New(app.recoverPanic, app.logRequest, commonHeaders)
+	// Return the 'standard' middleware chain followed by the servemux.
+	return standard.Then(mux)
 }
--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module faith.practice
 
 go 1.25.5
 
-require github.com/go-sql-driver/mysql v1.10.1
+require (
+	github.com/go-sql-driver/mysql v1.10.1
+	github.com/justinas/alice v1.2.0
+)
 
 require filippo.io/edwards25519 v1.2.0 // indirect

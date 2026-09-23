d8e217b58a36d0771d92c773792aeed37106cb64
Faith Johnson <ermac_4life@yahoo.com>
2026-09-23 10:32:52 +0100

    User authentication setup

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -23,6 +23,7 @@ import (
 type application struct {
 	logger         *slog.Logger
 	snippets       *models.SnippetModel
+	users          *models.UserModel
 	sessionManager *scs.SessionManager
 }
 
@@ -63,6 +64,7 @@ func main() {
 	app := &application{
 		logger:         logger,
 		snippets:       &models.SnippetModel{DB: db},
+		users:          &models.UserModel{DB: db},
 		sessionManager: sessionManager,
 	}
 
